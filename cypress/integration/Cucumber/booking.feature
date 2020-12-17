d3fc49b4661cd206fe582e13a35781244c3a5360
Kateryna Buchkovska <kbuchkovska@solvd.com>
2020-12-17 17:41:37 +0200

    Fix Cucumber tests

--- cypress/integration/Cucumber/booking.feature
+++ cypress/integration/Cucumber/booking.feature
@@ -5,16 +5,32 @@ Feature: BookFlightFeature
         Given I visit PHPTravelers site 
 
     Scenario: Book flight as logged in user
-        Given I log in 
-        Given I open flight search form
-        Given I search for flight
-        Given I select first flight
-        Given I submit passanger personal info
-        Then I should see message about unpaid booking
+        When I open account dropdown
+        Then I open login page
+        When I fill in login form
+        Then I login
+        When I open home page
+        Then I open flight form
+        When I enter city from and city to
+        And I select custom date
+        And I select business class
+        And I increase passangers quantity
+        Then I start search
+        When I select first flight result
+        Then I fill in passanger form
+        And I complete booking
+        When I open invoice page
+        Then I check if booking is unpaid
 
     Scenario: Book flight as guest
-        Given I open flight search form
-        Given I search for flight
-        Given I select first flight
-        Given I submit guest info
-        Then I should see message about unpaid booking
+        Then I open flight form
+        When I enter aeroport from and aeroport to
+        And I select current date
+        And I select business class
+        And I increase passangers quantity
+        Then I start search
+        When I select first flight result
+        Then I fill in guest form
+        And I complete booking
+        When I open invoice page
+        Then I check if booking is unpaid
--- cypress/integration/Cucumber/booking/booking.js
+++ cypress/integration/Cucumber/booking/booking.js
@@ -22,145 +22,228 @@ before(() => {
     })
 })
 
-//open site
+function selectDay(futureDay, futureMonth, futureYear){
+  cy.get('div.datepicker.-bottom-left-.-from-bottom-.active')
+      .find('.datepicker--nav-title')
+      .then(date => {
+        if(!date.text().includes(futureMonth) || !date.text().includes(futureYear)){
+          cy.get('div.datepicker.-bottom-left-.-from-bottom-.active > nav > div[data-action=next]')
+            .click()
+            selectDay(futureDay, futureMonth, futureYear)
+            } else {
+                cy.get('div[class^="datepicker--cell datepicker--cell-day"][data-date]')
+                  .contains(futureDay)
+                  .click()
+                }
+        })
+}
+
 Given('I visit PHPTravelers site', () => {
-    cy.visit('http://www.phptravels.net/home')
+  cy.visit('https://www.phptravels.net/home')
 })
 
-//fill log in form and login
-Given('I log in', () => {
-    cy.contains('div', 'My Account')
-      .click()
-    cy.contains('Login')
-      .click()
-    cy.contains('Email')
-      .type(user.email)
-    cy.contains('Password')
-      .type(user.password)
-    cy.contains('button', 'Login')
-      .click()
-      cy.get('.text-align-left').then(label => {
-        expect(label.text()).to.equal('Hi, ' + user.firstName + ' ' + user.lastName)
-    })
+When('I open account dropdown', () => {
+  cy.contains('div', 'My Account')
+    .click()
 })
 
-//open home page, flight search form
-Given('I open flight search form', () => {
-    cy.get('[title="home"]')
-      .click()
-    cy.url()
-      .should('eq', 'https://www.phptravels.net/home')
-
-    cy.get('[data-name="flights"]')
+Then('I open login page', () => {
+  cy.contains('Login')
       .click()
 })
 
-//fill flight form and start searching
-Given('I search for flight', () => {
-    cy.get('div.form-icon-left.flightclass > div.chosen-container.chosen-container-single.chosen-container-single-nosearch')
-      .click()
-    cy.get('ul.chosen-results')
-      .should('be.visible')
-      .find('li[class^="active-result"]')
-      .contains('Business')
-      .click()
-    
-    cy.get('a.chosen-single')
-      .should('contain', 'Business')
+When('I open home page', () => {
+  cy.get('[title="home"]')
+    .click()
+        
+  cy.url()
+    .should('eq', 'https://www.phptravels.net/home')
+})
 
+And('I select business class', () => {
+  cy.get('div.form-icon-left.flightclass > div.chosen-container.chosen-container-single.chosen-container-single-nosearch')
+    .click()
+  cy.get('ul.chosen-results')
+    .should('be.visible')
+    .find('li[class^="active-result"]')
+    .contains('Business')
+    .click()
+  
+  cy.get('a.chosen-single')
+    .should('contain', 'Business')
+})
+
+And('I select custom date', () => {
     cy.get('#FlightsDateStart')
       .click()
-    cy.get('div.datepicker.-bottom-left-.-from-bottom-.active')
-      .should('be.visible')
-      .find('div.datepicker--cell.datepicker--cell-day.-current-')
-      .click()
-    
-    cy.get('#s2id_location_from')
+
+    selectDay(flight.futureDay, flight.futureMonth, flight.futureYear )
+      
+    cy.get('#FlightsDateStart')
+      .should('not.be.empty')
+})
+
+Given('I select current date', () => {
+  cy.get('#FlightsDateStart')
+    .click()
+
+  cy.get('div.datepicker.-bottom-left-.-from-bottom-.active')
+    .should('be.visible')
+    .find('div.datepicker--cell.datepicker--cell-day.-current-')
+    .click()
+})
+
+When('I enter aeroport from and aeroport to', () => {
+  cy.get('#s2id_location_from')
       .type(flight.aeroportFrom)
     cy.get('.select2-search > .select2-focused')
       .wait(1000)
       .type('{enter}')
+      
+  cy.get('#s2id_location_to')
+    .type(flight.aeroportTo)
+  cy.get('.select2-search > .select2-focused')
+    .wait(1000)
+    .type('{enter}')
+})
 
+When('I enter city from and city to', () => {
+  cy.get('#s2id_location_from')
+      .type(flight.cityFrom)
+  cy.get('.select2-results')
+    .then(list => {
+      if(list.length > 1){
+        cy.get('li.select2-results-dept-0')
+          .last()
+          .click()
+      } else {
+          cy.get('li.select2-results-dept-0')
+            .first()
+            .click()
+          }
+      })
     cy.get('#s2id_location_to')
-      .type(flight.aeroportTo)
-    cy.get('.select2-search > .select2-focused')
-      .wait(1000)
-      .type('{enter}')
-    
-    cy.get('input[name="fadults"] + span > button')
-      .contains('+')
-      .click()
-    
-    cy.get('form[name="flightmanualSearch"]')
-      .submit()
-    cy.url().should('contain', 'flights/search')
-    
-})
-
-//book first flight
-Given('I select first flight', () => {
-    cy.get('#LIST')
-      .find('form')
-      .first()
-      .submit()
-        
-    cy.url().should('contain', 'flights/book')
-})
-
-//fill passanger form and submit
-Given('I submit passanger personal info', () => {
-    cy.get('#passenger_name_0')
-      .click({force:true})
-      .type(user.name)
-    cy.get('#passenger_age_0')
-      .click({force:true})
-      .type(user.age)
-    cy.get('#passenger_passport_0')
-      .click({force:true})
-      .type(user.passportNo) 
-    cy.get('button.btn.btn-success.btn-lg.btn-block.completebook')
-      .click()
-})
-
-Given('I submit guest info', () => {
-    cy.get('input[name="firstname"]')
-      .click({force:true})
-      .type(guest.firstName)
-    cy.get('input[name="lastname"]')
-      .click({force:true})
-      .type(guest.lastName)
-    cy.get('input[name="email"]')
-      .click({force:true})
-      .type(guest.email)
-    cy.get('input[name="confirmemail"]')
-      .click({force:true})
-      .type(guest.email)
-    cy.get('input[name="phone"]')
-      .click({force:true})
-      .type(guest.phone)
-    cy.get('input[name="address"]')
-      .click({force:true})
-      .type(guest.address)
-    cy.get('div.chosen-container.chosen-container-single')
-      .click()
-      .should('have.attr', 'class', 'chosen-container chosen-container-single chosen-with-drop chosen-container-active')
-    cy.get('div.chosen-search')
-      .type(guest.address)
+      .type(flight.cityTo)
+    cy.get('.select2-results')
       .then(list => {
-        if(list.children().eq(guest.address)){
-          cy.get('li.active-result')
+        if(list.length > 1){
+          cy.get('li.select2-results-dept-0')
+            .last()
+            .click()
+        } else {
+          cy.get('li.select2-results-dept-0')
+            .first()
             .click()
         }
       })
-      cy.get('button.btn.btn-success.btn-lg.btn-block.completebook')
-      .click()
 })
 
-//check message about unpaid booking
-Given('I should see message about unpaid booking', () => {
-    cy.url()
-      .should('contain', 'invoice')
-    cy.get('.content')
-      .contains('Your booking status is Unpaid')
-      .should('be.visible')
+Then('I open flight form', () => {
+  cy.get('[data-name="flights"]')
+    .click()
 })
+
+And('I increase passangers quantity', () => {
+  cy.get('input[name="fadults"] + span > button')
+    .contains('+')
+    .click()
+})
+
+Then('I start search', () => {
+  cy.get('form[name="flightmanualSearch"]')
+    .submit()
+  cy.url()
+    .should('contain', 'flights/search')
+})
+
+Given('I open home page', () => {
+  cy.get('[title="home"]')
+      .click()
+    
+  cy.url()
+    .should('eq', 'https://www.phptravels.net/home')
+})
+
+When('I select first flight result', () => {
+  cy.get('#LIST')
+    .find('form')
+    .first()
+    .submit()
+        
+  cy.url().should('contain', 'flights/book')
+})
+
+When('I open invoice page', () => {
+  cy.url()
+    .should('contain', 'invoice')
+})
+
+Then('I check if booking is unpaid', () => {
+  cy.get('.content')
+    .contains('Your booking status is Unpaid')
+    .should('be.visible')
+})
+
+When('I fill in login form', () => {
+  cy.contains('Email')
+    .type(user.email)
+  cy.contains('Password')
+    .type(user.password)
+})
+
+Then('I login', () => {
+  cy.contains('button', 'Login')
+    .click()
+  cy.url()
+    .should('contain', 'account')
+})
+
+Then('I fill in passanger form', () => {
+  cy.get('#passenger_name_0')
+    .click({force:true})
+    .type(user.name)
+  cy.get('#passenger_age_0')
+    .click({force:true})
+    .type(user.age)
+  cy.get('#passenger_passport_0')
+    .click({force:true})
+    .type(user.passportNo) 
+})
+
+Given('I fill in guest form', () => {
+  cy.get('input[name="firstname"]')
+        .click({force:true})
+        .type(guest.firstName)
+      cy.get('input[name="lastname"]')
+        .click({force:true})
+        .type(guest.lastName)
+      cy.get('input[name="email"]')
+        .click({force:true})
+        .type(guest.email)
+      cy.get('input[name="confirmemail"]')
+        .click({force:true})
+        .type(guest.email)
+      cy.get('input[name="phone"]')
+        .click({force:true})
+        .type(guest.phone)
+      cy.get('input[name="address"]')
+        .click({force:true})
+        .type(guest.address)
+      cy.get('div.chosen-container.chosen-container-single')
+        .click()
+        .should('have.attr', 'class', 'chosen-container chosen-container-single chosen-with-drop chosen-container-active')
+      cy.get('div.chosen-search')
+        .type(guest.address)
+        .then(list => {
+          if(list.children().eq(guest.address)){
+            cy.get('li.active-result')
+              .click()
+          }
+        })
+})
+
+And('I complete booking', () => {
+  cy.get('button.btn.btn-success.btn-lg.btn-block.completebook')
+    .click()
+})
+
